4547a039431e4bb9ec8036ecbd99d6b5f034e0d5
juandrouville <jadrouville@gmail.com>
2023-04-26 15:03:26 -0300

    changes and add gif tie

--- src/Components/HaveTie/HaveTie.jsx
+++ src/Components/HaveTie/HaveTie.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './haveTieStyle.css';
+import gif from './tie.gif';
 
 function HaveTie (){
     return (
-        <div>
-            Empate!
+        <div className="conteinerTie">
+            <img src={gif}/>
+            <h1>Empate!</h1>
         </div>
     );
 };
